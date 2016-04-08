38a7727ebbd7e13b7d3c5657ff87a1ea4adea02f
Nathan Haskell <nbhas@Raven.(none)>
2016-04-08 14:22:32 -0700

    completed challenge 2/finding min and max values in an array

---
+++ minimumandmaximum/minimumandmaximum/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace minimumandmaximum
+{
+    class Program
+    {
+
+        //Write a program that takes input array daily temperatures and find the maximum and minimum values.
+
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+            Console.WriteLine("Enter 10 numbers and I will find the highest and lowest numbers entered: ");
+
+            for (int i = 0; i < 10; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int smallest = array[0];
+            for (int i=0; i<10; i++)
+            {
+                if(array[i]<smallest)
+                {
+                    smallest = array[i];
+                }
+            }
+
+            int largest = array[9];
+            for (int i=0; i<10; i++)
+            {
+                if(array[i]>largest)
+                {
+                    largest = array[i];
+                }
+            }
+            Console.WriteLine("The smallest number entered is {0}", smallest);
+            Console.WriteLine("The largest number entered is {0}", largest);
+            Console.Read();
+        }
+    }
+}
